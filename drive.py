1147072c63ec846ef30f0c9bafcb1d5fa9d1022f
powerlandteam <102792244+powerlandteam@users.noreply.github.com>
2022-04-02 11:59:29 +0530

    Update drive.py

--- drive.py
+++ drive.py
@@ -1,6 +1,44 @@
 
 import RPi.GPIO as GPIO
 import time
+import Adafruit_CharLCD as LCD    
+import Adafruit_MCP4725
+import time
+
+lcd_rs        = 25 
+lcd_en        = 24
+lcd_d4        = 23
+lcd_d5        = 17
+lcd_d6        = 18
+lcd_d7        = 22
+lcd_backlight = 4
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows    = 2
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+
+lcd.message('Circuit Digest')
+time.sleep(2.0)
+lcd.message('\nDAC using Rpi')
+time.sleep(5.0)
+lcd.clear()
+dac = Adafruit_MCP4725.MCP4725(address=0x60)
+
+while True:
+    for x in range(0,4097,150):
+        
+        print(x)
+        dac.set_voltage(x)
+        lcd.cursor_pos = (0,0)
+        lcd.message("DAC Value: " + str(x))
+        voltage = x/4096.0*5.0
+        lcd.message("\nAnalogVolt: %.2f" % voltage)
+        time.sleep(2)
+        lcd.clear()
+
 
 leftEn = 12			#	Purple
 rightEn = 13		#	Red
